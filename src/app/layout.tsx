c6ab349ec1cba77824f706150bee00dfee2e7833
unknown <gehlotmitesh63@gmail.com>
2024-11-24 10:02:41 +0530

    final touch complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <Authprovider>
       <body
-      className="flex flex-col h-screen w-screen"
+      className="flex flex-col h-screen w-screen overflow-x-hidden "
       >
         <Header/>
         {children}
